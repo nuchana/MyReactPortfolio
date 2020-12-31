7b99496979a93e454f06556934166df912242020
Nuchana <nuchana.johnson@gmail.com>
2020-12-31 12:59:36 -0500

    update portfolio.js

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './portfolio.css';
 
-
 class Portfolio extends Component {
     render() {
         return (
@@ -18,40 +17,45 @@ class Portfolio extends Component {
                         </div>
 
                         <div class="row">
-                                <div class="col-sm-2"> </div>
-                                <figure class="col-md-4">
-                                    <a href="https://nuchana.github.io/Awesome-weather-dashboard/"><img src="images/myapp.png"
-                                        class="img-thumbnail" alt="Weather App" style= {{ width: "370px", height: "200px" }}></img></a>
-                                        <figcaption class="figure-caption">Click image to Weather Forecast App.</figcaption>
-                                </figure>
+                            <div class="col-sm-2"> </div>
+                            <figure class="col-md-4">
+                                <a href="https://nuchana.github.io/Awesome-weather-dashboard/"><img src={process.env.PUBLIC_URL + '/forecast.png'}
+                                    class="img-thumbnail" alt="Weather App" style={{ width: "370px", height: "200px" }}></img></a>
+                                <figcaption class="figure-caption">Click image to Weather Forecast App.</figcaption>
+                            </figure>
 
-                                <figure class="col-md-4">
-                                    <a href="https://github.com/nuchana/OOP-team-generator"><img src="images/NK-team-demo.png"
-                                            class="img-thumbnail" alt="Team Generator App" style= {{ width: "370px", height: "200px" }}></img></a>
-                                            <figcaption class="figure-caption">Click image to Team Generator App.</figcaption>
+                            <figure class="col-md-4">
+                                <a href="https://github.com/nuchana/OOP-team-generator"><img src={process.env.PUBLIC_URL + '/teamdemo.png'}
+                                    class="img-thumbnail" alt="Team Generator App" style={{ width: "370px", height: "200px" }}></img></a>
+                                <figcaption class="figure-caption">Click image to Team Generator App.</figcaption>
 
-                                </figure>
+                            </figure>
                         </div>
                         <br></br>
 
                         <div class="row">
-                                <div class="col-sm-2"></div>
-                                <figure class="col-md-4">
-                                        <a href=" https://superzap-burger.herokuapp.com/burgers"><img src="images/burgerapp.png" class="img-thumbnail"
-                                        alt="Team Generator App" style= {{ width: "370px", height: "200px" }}></img></a>
-                                        <figcaption class="figure-caption">Click image to Burger App.</figcaption>
+                            <div class="col-sm-2"></div>
+                            <figure class="col-md-4">
+                                <a href=" https://superzap-burger.herokuapp.com/burgers"><img src={process.env.PUBLIC_URL + '/burgerapp.png'} class="img-thumbnail"
+                                    alt="Team Generator App" style={{ width: "370px", height: "200px" }}></img></a>
+                                <figcaption class="figure-caption">Click image to Burger App.</figcaption>
 
-                                </figure>
+                            </figure>
+
+                            <figure class="col-md-4">
+                                <a href="https://nuchana.github.io/Employee-Online-Yellowbook/"><img src={process.env.PUBLIC_URL + '/employeeReact.png'} class="img-thumbnail"
+                                    alt="Employee Directory React App" style={{ width: "370px", height: "200px" }}></img></a>
+                                <figcaption class="figure-caption">Click image to Employee Directory React App.</figcaption>
+
+                            </figure>
 
                         </div>
 
-
                     </div>
+
+
                 </div>
-
-
             </div>
-            
 
         )
     }
