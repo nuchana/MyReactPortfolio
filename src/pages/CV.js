a48d50fc3f3a0fb24781ba8e844d3d668a780925
Nuchana <nuchana.johnson@gmail.com>
2020-12-31 15:14:12 -0500

    github deploy

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import "./cv.css"
+import { NavLink } from "react-router-dom";
 
 class Resume extends Component {
 
@@ -10,6 +11,7 @@ class Resume extends Component {
         <h1 className="text-center"><strong>Curriculum Vitae</strong></h1>
         <div id="cvDownload"> 
         <a class="nav-link" href="public/resume.png" download src="/public/resume.png">Download Resume</a>
+       
         </div>
         <a href="public/resume.png"><img id="cvImg" alt= "CV" src={process.env.PUBLIC_URL + '/resume.png'}/></a>
       </div>
