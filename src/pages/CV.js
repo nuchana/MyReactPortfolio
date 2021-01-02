01e3a7695a61a96b34f3e5beea5ce6ff514b7772
Nuchana <nuchana.johnson@gmail.com>
2021-01-02 17:34:56 -0500

    fix download resume

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import "./cv.css"
 
 
+
 class Resume extends Component {
 
   render() {
@@ -10,8 +11,7 @@ class Resume extends Component {
       <div>
         <h1 className="text-center"><strong>Curriculum Vitae</strong></h1>
         <div id="cvDownload"> 
-        <a class="nav-link" href="public/resume.png" download src="/public/resume.png">Download Resume</a>
-        
+        <a class="nav-link" href={process.env.PUBLIC_URL + '/NKresume.pdf'} download>Download Resume</a>
         </div>
         <a href="public/resume.png"><img id="cvImg" alt= "CV" src={process.env.PUBLIC_URL + '/resume.png'}></img></a>
       </div>
